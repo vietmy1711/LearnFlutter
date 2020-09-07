df897204d876b0d273570e2f32025cd495798c6f
Nguyen Viet My <zzvietmyzz@gmail.com>
2020-09-07 15:04:36 +0700

    Output widgets conditionally.

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -14,47 +14,51 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final questions = const [
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answers': ['Blue', 'Black', 'Green', 'White']
+    },
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answers': ['Rabbit', 'Snake', 'Cock', 'Pussy']
+    },
+    {
+      'questionText': 'Who\'s the most handsome man in the world?',
+      'answers': ['My', 'Dan', 'My Dan', 'My dep trai']
+    },
+  ];
   var _index = 0;
 
   void _answerQuestion() {
     setState(() {
-      if (_index == 2) {
-        _index = 0;
-        return;
-      }
+      // if (_index == questions.length - 1) {
+      //   _index = 0;
+      //   return;
+      // }
       _index++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    const questions = [
-      {
-        'questionText': 'What\'s your favorite color?',
-        'answers': ['Blue', 'Black', 'Green', 'White']
-      },
-      {
-        'questionText': 'What\'s your favorite animal?',
-        'answers': ['Rabbit', 'Snake', 'Cock', 'Pussy']
-      },
-      {
-        'questionText': 'Who\'s the most handsome man in the world?',
-        'answers': ['My', 'Dan', 'My Dan', 'My dep trai']
-      },
-    ];
     return MaterialApp(
         home: Scaffold(
             appBar: AppBar(
               title: Text("Alo"),
               backgroundColor: Colors.greenAccent,
             ),
-            body: Column(
-              children: [
-                Question(questions[_index]['questionText']),
-                ...(questions[_index]['answers'] as List<String>)
-                    .map((answer) => Answer(answer, _answerQuestion))
-                    .toList()
-              ],
-            )));
+            body: _index < questions.length
+                ? Column(
+                    children: [
+                      Question(questions[_index]['questionText']),
+                      ...(questions[_index]['answers'] as List<String>)
+                          .map((answer) => Answer(answer, _answerQuestion))
+                          .toList()
+                    ],
+                  )
+                : Center(
+                    child: Text("No more"),
+                  )));
   }
 }
